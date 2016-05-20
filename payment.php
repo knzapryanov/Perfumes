019120eb8c5d2507c298185d418bd2673c69eb29
knzapryanov <knzapryanov@gmail.com>
2016-05-20 14:10:18 +0300

    Initial commit

---
+++ payment.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+include "template.class.php";
+
+$template = new Template;
+$template->load("template.html");
+$template->replace("MAIN_CONTENT_TITLE", "");
+$template->replace("MAIN_CONTENT",file_get_contents('payment.html'));
+$template->publish();
+
+?>
---
+++ template.class.php
@@ -0,0 +1,19 @@
+<?php
+
+class Template {
+   public $template;
+   
+   function load($filepath) {
+      $this->template = file_get_contents($filepath);
+   }
+
+   function replace($var, $content) {
+      $this->template = str_replace("--$var--", $content, $this->template);
+   }
+
+   function publish() {
+      eval("?>".$this->template."<?php ");
+   }
+}
+
+?>
