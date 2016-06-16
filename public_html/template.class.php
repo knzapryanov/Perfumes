4ae4c9f0e8c7089484a8c56dd50ca0997803437b
knzapryanov <knzapryanov@gmail.com>
2016-06-16 16:27:48 +0300

    Remove unnecessary files

--- public_html/template.class.php
+++
@@ -1,19 +0,0 @@
-<?php
-
-class Template {
-   public $template;
-   
-   function load($filepath) {
-      $this->template = file_get_contents($filepath);
-   }
-
-   function replace($var, $content) {
-      $this->template = str_replace("--$var--", $content, $this->template);
-   }
-
-   function publish() {
-      eval("?>".$this->template."<?php ");
-   }
-}
-
-?>
